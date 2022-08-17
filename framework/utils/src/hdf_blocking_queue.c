27ae87173c7876f2f1aa6e85e6288dbaf52e9b7a
zhang <zhangfengxi@huawei.com>
2022-08-17 15:12:11 +0800

    fix:clear the reviewbot warning of the HDF module on the master branch
    Signed-off-by: zhang <zhangfengxi@huawei.com>

--- framework/utils/src/hdf_blocking_queue.c
+++ framework/utils/src/hdf_blocking_queue.c
@@ -55,7 +55,7 @@ void *HdfBlockingQueueGet(struct HdfBlockingQueue *queue)
 }
 
 
-void *HdfBlockingQueueFind(struct HdfBlockingQueue *queue, long matchKey, SListComparer comparer)
+void *HdfBlockingQueueFind(const struct HdfBlockingQueue *queue, long matchKey, SListComparer comparer)
 {
     void *matchData = NULL;
     struct HdfSListIterator it;
