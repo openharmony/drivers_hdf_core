2c659c1e694f778ec4a12723a01bbf4b0af05cb5
LiteWang <wwz529247756@163.com>
2022-08-23 22:50:40 +0800

    fix warning: 'return' with a value, in function returning void
    Signed-off-by: LiteWang <wwz529247756@163.com>

--- adapter/platform/gpio/gpio_bes.c
+++ adapter/platform/gpio/gpio_bes.c
@@ -343,7 +343,7 @@ static void GpioDriverRelease(struct HdfDeviceObject *device)
     gpioCntlr = GpioCntlrFromHdfDev(device);
     if (gpioCntlr == NULL) {
         HDF_LOGE("GpioCntlrFromHdfDev fail\r\n");
-        return HDF_DEV_ERR_NO_DEVICE_SERVICE;
+        return;
     }
 
     gpioCntlr->ops = NULL;
