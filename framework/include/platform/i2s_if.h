0bdac97b1f81f6a4b6447576220b121f191caf7b
jiaziyangnewer <jiaziyang1@huawei.com>
2023-01-06 15:07:58 +0800

    fix:Replace Sensitive Words.
    Signed-off-by: jiaziyangnewer <jiaziyang1@huawei.com>

--- framework/include/platform/i2s_if.h
+++ framework/include/platform/i2s_if.h
@@ -140,20 +140,20 @@ enum I2slFsSel {
 
 
 struct I2sCfg {
-    enum I2sSampleRate sampleRate;     /**< I2S sample rate, 8k,16k,32k... */
-    enum I2sWordWidth width;            /**< I2S word width, 8bit,16bit,20bit,24bit... */
-    enum I2sMode  mode;                /**< I2S mode, host/standby, tx/rx */
-    enum I2sPcmFscDelay syncMode;      /**< pcm mode, FSC starts location */
-    enum I2sLoopMode  loopMode;        /**< is loopback */
-    enum I2sProtocolType type;         /**< I2S protocol type */
-    enum I2sChannel  channel;
-    enum I2sFrameLen  frameLen;
-    enum I2sDataSel  dataSel;
-    enum I2sChannelMode channelMode;   /**< I2S mode,data channel */
+    enum I2sSampleRate sampleRate;    /**< I2S sample rate, 8k,16k,32k... */
+    enum I2sWordWidth width;          /**< I2S word width, 8bit,16bit,20bit,24bit... */
+    enum I2sMode mode;                /**< I2S mode, host/standby, tx/rx */
+    enum I2sPcmFscDelay syncMode;     /**< pcm mode, FSC starts location */
+    enum I2sLoopMode loopMode;        /**< is loopback */
+    enum I2sProtocolType type;        /**< I2S protocol type */
+    enum I2sChannel channel;
+    enum I2sFrameLen frameLen;
+    enum I2sDataSel dataSel;
+    enum I2sChannelMode channelMode;  /**< I2S mode,data channel */
     uint8_t samplePrecision;          /**< bit */
     enum I2sChannelIfMode channelIfMode;
-    uint32_t mclk;                     /**< KHZ */
-    uint32_t bclk;                     /**< KHZ */
+    uint32_t mclk;                    /**< KHZ */
+    uint32_t bclk;                    /**< KHZ */
     enum I2sWriteChannel writeChannel;
     enum I2slFsSel i2slFsSel;
     uint8_t Reserved[2];
