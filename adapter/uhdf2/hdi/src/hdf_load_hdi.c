f223a75ab2996ea2fe2f7dab98caffbba9240eca
wen_tengye <wen_tengye@hoperun.com>
2024-01-26 14:23:49 +0800

    新增dayu800产品audio代码适配
    Signed-off-by: wentengye <wen_tengye@hoperun.com>

--- adapter/uhdf2/hdi/src/hdf_load_hdi.c
+++ adapter/uhdf2/hdi/src/hdf_load_hdi.c
@@ -23,7 +23,7 @@
 
 #define HDF_LOG_TAG dev_load_hdi
 
-#ifdef __ARM64__
+#if defined(__ARM64__) || defined(__RISCV64__)
 #define HDI_SO_PATH HDF_LIBRARY_DIR"64"
 #else
 #define HDI_SO_PATH HDF_LIBRARY_DIR
