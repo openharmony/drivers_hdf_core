0a3bce9ddb2ee653cbb74bbec2905c7f61878c44
huyx <huyuxiang8@h-partners.com>
2024-09-03 20:31:52 +0800

    基础编码优化
    Signed-off-by: huyx <huyuxiang8@h-partners.com>

--- adapter/khdf/linux/model/camera/src/sg_dma.c
+++ adapter/khdf/linux/model/camera/src/sg_dma.c
@@ -89,7 +89,7 @@ static int32_t SgAllocCompacted(struct SgDmaBuffer *buf, gfp_t gfpFlags)
         for (i = 0; i < (1 << order); i++) {
             buf->pages[lastPage++] = &pages[i];
         }
-        if (size < PAGE_SIZE << order) {
+        if (size < (PAGE_SIZE << order)) {
             HDF_LOGE("%s: size invalid", __func__);
             return -EINVAL;
         }
