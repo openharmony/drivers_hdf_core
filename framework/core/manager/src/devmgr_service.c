29dabc51c80d7f2516678e8597427ad743464171
zh_zhao <zhaozhenhui2@h-partners.com>
2026-08-20 10:34:00 +0800

    update: 更新文件 devmgr_service.c
    Signed-off-by: zh_zhao <zhaozhenhui2@h-partners.com>

--- framework/core/manager/src/devmgr_service.c
+++ framework/core/manager/src/devmgr_service.c
@@ -289,7 +289,7 @@ static int DevmgrServiceDetachDevice(struct IDevmgrService *inst, devid_t devid)
     }
     tokenClnt = CONTAINER_OF(tokenClntNode, struct DeviceTokenClnt, node);
     HdfSListRemove(&hostClnt->devices, &tokenClnt->node);
-    DeviceTokenClntFreeInstance(tokenClnt);
+  DeviceTokenClntFreeInstance(tokenClnt);
     return HDF_SUCCESS;
 }
 
