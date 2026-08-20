3f62aec7c9811f5f7024d4a42a0ec6d38331d4e4
zh_zhao <zhaozhenhui2@h-partners.com>
2026-08-20 03:15:59 +0000

    告警修改：高优先级
    Signed-off-by: zh_zhao <zhaozhenhui2@h-partners.com>

--- framework/utils/src/hdf_sbuf.c
+++ framework/utils/src/hdf_sbuf.c
@@ -511,7 +511,7 @@ struct HdfSBuf *HdfSbufMove(struct HdfSBuf *sbuf)
         OsalMemFree(newBuf);
         return NULL;
     }
-  newBuf->type = sbuf->type;
+    newBuf->type = sbuf->type;
     return newBuf;
 }
 
