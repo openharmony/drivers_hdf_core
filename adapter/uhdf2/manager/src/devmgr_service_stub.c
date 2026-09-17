4962bc727b8afc0b964a3419f87f73a6d47e1bcd
lubaoliang <lubaoliang1@h-partners.com>
2026-09-17 09:12:30 +0800

    修复wifi连接crash问题
    Signed-off-by: lubaoliang <lubaoliang1@h-partners.com>

--- adapter/uhdf2/manager/src/devmgr_service_stub.c
+++ adapter/uhdf2/manager/src/devmgr_service_stub.c
@@ -71,11 +71,6 @@ static int32_t DevmgrServiceStubDispatchAttachDeviceHost(struct IDevmgrService *
         return HDF_FAILURE;
     }
     struct IDevHostService *hostIf = DevHostServiceProxyObtain(hostId, service);
-    if (hostIf == NULL) {
-        HDF_LOGE("%{public}s: failed to obtain host service proxy", __func__);
-        HdfRemoteServiceRecycle(service);
-        return HDF_ERR_MALLOC_FAIL;
-    }
     DevmgrServicehostClntGetPid(devmgrSvc, (uint16_t)hostId);
     return devmgrSvc->AttachDeviceHost(devmgrSvc, hostId, hostIf);
 }
